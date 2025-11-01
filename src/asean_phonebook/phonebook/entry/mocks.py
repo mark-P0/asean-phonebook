ddf132219dc19b2b6bcfcb66e6ba0e8e98b3af22
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2025-11-02 02:10:34 +0800

    add mock entry naive tests

--- src/asean_phonebook/phonebook/entry/mocks.py
+++ src/asean_phonebook/phonebook/entry/mocks.py
@@ -1,3 +1,4 @@
+from pprint import pprint
 from asean_phonebook.phonebook.entry.entry import (
     PhonebookEntry,
     PhonebookEntryCountryCode,
@@ -49,3 +50,33 @@ class MockPhonebookEntry:
         area_code=2,
         number=9998765,
     )
+
+
+if __name__ == "__main__":
+    entries = [
+        MockPhonebookEntry.SUKARNO_LEE,
+        MockPhonebookEntry.JULIANA_DELA_CRUZ,
+        MockPhonebookEntry.SAWADI_KRAP,
+        MockPhonebookEntry.JOHN_SAINT,
+    ]
+
+    _ = "\n\n\n\n".join(
+        "\n\n".join(
+            [
+                str(entry),
+                entry.as_edit_str,
+                entry.as_search_str,
+            ]
+        )
+        for entry in entries
+    )
+    _ = {
+        entry.student_number: dict(
+            __str__=str(entry),
+            edit=entry.as_edit_str,
+            search=entry.as_search_str,
+        )
+        for entry in entries
+    }
+
+    pprint(_, width=100)
